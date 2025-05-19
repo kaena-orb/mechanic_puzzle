abfaff49e23372671607b2f13aa76eabb22233a3
Hernan <pradoh97@gmail.com>
2025-05-19 12:23:31 -0300

    Move repeated logic into own method

--- toggle.gd
+++ toggle.gd
@@ -1,29 +1,34 @@
 extends StaticBody2D
 class_name Toggle
 
+var toggled : bool = false
 var bodies_in_place : Array
-signal toggle_pressed
-signal toggle_released
+var one_shot : bool = false
+signal toggle_pressed(emiter)
+signal toggle_released(emiter)
+
 
 # Called when the node enters the scene tree for the first time.
 func _ready():
 	pass # Replace with function body.
 
-func toggle_on():
-	pass
+func release():
+	$AnimationPlayer.play("RESET")
+	toggle_released.emit(self)
+	toggled = false
+
 
 func add_body(body):
-	if not bodies_in_place.size():
+	if not toggled or not bodies_in_place.size():
 		$AnimationPlayer.play("press")
-		toggle_pressed.emit()
+		toggle_pressed.emit(self)
+		toggled = true
 	bodies_in_place.append(body)
 
 func remove_body(body):
 	bodies_in_place.erase(body)
-	if not bodies_in_place.size():
-		$AnimationPlayer.play("RESET")
-		toggle_released.emit()
-
+	if not bodies_in_place.size() and not one_shot:
+		release()
 func _on_object_detection_area_entered(area):
 	add_body(area)
 
