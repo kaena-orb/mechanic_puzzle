af0352298230068122e348634dfc7eba5365cf81
Hernan <pradoh97@gmail.com>
2025-05-19 12:23:10 -0300

    Add toggle groups signal and move repeated logic into methods

--- platform.gd
+++ platform.gd
@@ -1,29 +1,45 @@
 extends StaticBody2D
 class_name Platform
 
-@export var hiddable : bool
+@export var hide_on_success : bool
+@export var toggle_group : ToggleGroup
 @export var togglers : Array[Toggle]
 var togglers_pressed : int
 # Called when the node enters the scene tree for the first time.
 
 func _ready():
-	if hiddable:
+	wire_signals()
+	if not hide_on_success and togglers.size():
 		hide_platform()
+
+
+func wire_signals():
 	for toggler in togglers:
 		toggler.toggle_pressed.connect(enable)
 		toggler.toggle_released.connect(disable)
+	if toggle_group:
+		toggle_group.toggle_group_solved.connect(enable)
 
-func enable():
+func enable(_emitter = null):
 	togglers_pressed += 1
-	if togglers_pressed == togglers.size():
-		$CollisionShape2D.set_deferred("disabled", false)
-		$AnimationPlayer.play("show")
+	if togglers_pressed == togglers.size() or not togglers.size() and toggle_group:
+		if hide_on_success:
+			hide_platform()
+		else:
+			show_platform()
 
-func disable():
+func disable(_emitter = null):
 	togglers_pressed -= 1
-	hide_platform()
+	if hide_on_success:
+		show_platform()
+	else:
+		hide_platform()
 
 func hide_platform():
 	if not $CollisionShape2D.disabled:
 		$CollisionShape2D.set_deferred("disabled", true)
 		$AnimationPlayer.play("hide")
+
+func show_platform():
+	$CollisionShape2D.set_deferred("disabled", false)
+	$AnimationPlayer.play("show")
